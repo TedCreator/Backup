7d8f2a32d82fae0be832f76bf36ce0ed3f6858a3
mfwRogue <takahori2@gmail.com>
2023-07-12 22:20:57 -0400

    Cleaned up some, & changed game vars to be entry

--- backupadv.cpp
+++ backupadv.cpp
@@ -11,24 +11,26 @@ using namespace std;
 fstream root;
 string userenv = getenv("USERPROFILE");
 string backupdir = R"(C:\Games\BACKUP\)";
-string rootdir = backupdir + "gamelocations.txt";
+string rootdir = backupdir + "entrylocations.txt";
 string backuploc = "\"" + backupdir + "\"";
 
-class Game {
+class Entry {
     public: 
         string name;
         int line;
         string savepath;
-        Game(){
+        Entry(){
             line = 0;
         }
-        Game(int place, string game, string path){
-            name = game;
+        ~Entry(){
+        }
+        Entry(int place, string entry, string path){
+            name = entry;
             line = place;
             savepath = path;
         }
-        Game(string game, string path){
-            name = game;
+        Entry(string entry, string path){
+            name = entry;
             line = -1;
             savepath = path;
         }
@@ -41,17 +43,14 @@ class Game {
         string getPath(){
             return savepath;
         }
-        void setName(string game){
-            name = game;
+        void setName(string entry){
+            name = entry;
         }
         void setLine(int place){
             line = place;
         }
         void setPath(string path){
             savepath = path;
-        }
-        ~Game(){
-
         }
         string toCommit(){
             return name + "{" + savepath + "}";
@@ -63,7 +62,8 @@ class Game {
             return ret;
         }
 };
-vector<Game> games;
+
+vector<Entry> entrys;
 string stlow(string str){
     char ch;
     string ret = "";
@@ -72,70 +72,69 @@ string stlow(string str){
     }
     return ret;
 }
-
-Game searchGames(string game){
-   for(auto & element : games){
-        if(stlow(element.getName()) == stlow(game)){
+Entry searchEntrys(string entry){
+   for(auto & element : entrys){
+        if(stlow(element.getName()) == stlow(entry)){
             return element;
         }
     }
-    return Game(0, "", "");
+    return Entry(0, "", "");
 }
-int findPosInVector(Game game){
-    for(int i = 0; i < games.size(); i++){
-        if(games.at(i).getName() == game.getName()){
+int findPosInVector(Entry entry){
+    for(int i = 0; i < entrys.size(); i++){
+        if(entrys.at(i).getName() == entry.getName()){
             return i;
         }
     }
     return -1;
 }
-Game editGame(Game game){
+Entry editEntry(Entry entry){
     string newName;
     string newPath;
-    cout << "To keep name, enter x. " << endl << "Edit the game name: ";
+    cout << "To keep name, enter x. " << endl << "Edit the Entry name: ";
     cin >> newName;
     if(newName != "x"){
-        game.setName(newName);
+        entry.setName(newName);
     }
-    cout << "To keep path, enter x." << endl << "Edit the game path: ";
+    cout << "To keep path, enter x." << endl << "Edit the Entry path: ";
     cin >> newPath;
     if(newPath != "x"){
-        game.setPath(newPath);
+        entry.setPath(newPath);
     }
-    cout << "New game entry is: " << game.toCommit() << endl;
-    return game;
+    cout << "New entry is: " << entry.toCommit() << endl;
+    return entry;
 }
-void addGame(){
+void addEntry(){
     string input;
-    string game;
-    Game newGame;
-    cout << "Enter the name of the game: ";
+    string entry;
+    Entry newEntry;
+    cout << "Enter the name of the entry: ";
     cin.ignore();
     getline(cin, input);
-    newGame.setName(input);
-    Game searchedGame = searchGames(input);
-    if(searchedGame.getName().size() < 1){
-        newGame.setName(input);
-        cout << "Enter game save folder you want backed up: ";
+    newEntry.setName(input);
+    Entry searchedEntry = searchEntrys(input);
+    if(searchedEntry.getName().size() < 1){
+        newEntry.setName(input);
+        cout << "Enter file path to be backed up: ";
         getline(cin, input);
-        newGame.setPath(input);
-        games.push_back(newGame);
+        newEntry.setPath(input);
+        entrys.push_back(newEntry);
     } else { 
-        cout << "Game " + input + " already exists with path: " << searchedGame.getPath() << endl;
+        cout << "Entry " + input + " already exists with path: " << searchedEntry.getPath() << endl;
         cout << "Editing." << endl;
-        newGame = editGame(searchedGame);
-        games.at(findPosInVector(searchedGame)) = newGame;
+        newEntry = editEntry(searchedEntry);
+        entrys.at(findPosInVector(searchedEntry)) = newEntry;
     }
 }
-void deleteGame(){
-    string game;
-    cout << "What game do you want to delete? " << endl << "Enter the name: ";
-    cin >> game;
-    game = stlow(game);
-    cout << game;
-    // for(auto & element : games){
-    //     if(element.getName() == game){
-    //         games.erase(games.begin()+element.getLine() - 1);
+void deleteEntry(){
+    string entry;
+    cout << "What entry do you want to delete? " << endl << "Enter the name: ";
+    cin >> entry;
+    entry = stlow(entry);
+    cout << entry;
+    // for(auto & element : entrys){
+    //     if(element.getName() == entry){
+    //         entrys.erase(entrys.begin()+element.getLine() - 1);
     //     }
     // }
 }
@@ -151,81 +150,81 @@ void fillVector(){
         name = line.substr(0, pos);
         path = line.substr(pos + 1);
         path = path.substr(0, path.size() - 1);
-        games.push_back(Game(count, name, path));
+        entrys.push_back(Entry(count, name, path));
         count++;
     }
     root.close();
 }
 void commit(){
     root.open(rootdir, ios::out);
-    for(auto & element : games){
+    for(auto & element : entrys){
         root.clear();
         root << element.toCommit() << endl;
     }
     root.close();
 }
-void zipUp(string saveloc, string gameName){
+void zipUp(string saveloc, string entryName){
     time_t tt = chrono::system_clock::to_time_t(chrono::system_clock::now());
     tm utc_tm = *gmtime(&tt);
 
-    //file name idea is "nameOfGame month-day-year-?h?m?s"
-    string fileName = gameName + " " + to_string(utc_tm.tm_mon + 1) + "-" + to_string(utc_tm.tm_mday) + "-" + to_string(utc_tm.tm_year + 1900) 
+    //file name idea is "nameOfEntry month-day-year-?h?m?s"
+    string fileName = entryName + " " + to_string(utc_tm.tm_mon + 1) + "-" + to_string(utc_tm.tm_mday) + "-" + to_string(utc_tm.tm_year + 1900) 
                 + "-" + to_string(utc_tm.tm_hour-5) + "h" + to_string(utc_tm.tm_min) + "m" + to_string(utc_tm.tm_sec) + "s";
-    
+
     //change to use tar command
     string cmd = "cd \"" + saveloc + "\" & 7z a \"" + fileName + ".zip\"";
     cmd += " & move \"" + fileName + ".zip\" " + backupdir;
     system(cmd.c_str());
 }
 int main(){
-    cout << rootdir;
+    cout << rootdir << endl;
     string userOption = " ";
     fillVector();
-    for(int i = 0; i < games.size(); i++){
-        cout << games.at(i).toString() << endl;
+    for(int i = 0; i < entrys.size(); i++){
+        cout << entrys.at(i).toString() << endl;
     }
 
-    cout << endl << "0. Add/Edit Game" << endl << "~. to delete a game" << endl << "s. to save changes" << endl 
-         << "x. to save and quit" << endl << "q. to quit without saving" << endl << "a. to backup all to folder" << endl;
+    cout << endl << "0. Add/Edit Entry" << endl << "~. to delete a entry" << endl << "s. to save changes" << endl 
+         << "q. to save and quit" << endl << "x. to quit without saving" << endl << "a. to backup all to folder" << endl;
     while(userOption != "q"){
         cout << "Select an option: ";
         cin >> userOption;
         userOption = stlow(userOption);
         switch(userOption.at(0)){
             case '0':
-                addGame();
-                for(Game i: games){cout << i.toCommit() << endl;}
+                addEntry();
+                for(Entry i: entrys){cout << i.toCommit() << endl;}
             break;
             case '~':
-                deleteGame();
+                deleteEntry();
             break;
             case 'a':
-                for(Game i: games){
+                for(Entry i: entrys){
                     
                     zipUp(i.getPath(), i.getName());
                 }
-                cout << endl << "Backed up game(s): ";
-                for(Game i : games){ 
+                cout << endl << "Backed up entry(s): ";
+                for(Entry i : entrys){ 
                     cout << i.getName() << " ";
                 }
                 cout << "to path " << backupdir << endl;
             break;
-            case 'x':
+            case 'q':
                 commit();
                 exit(0);
             case 's':
             commit();
             break;
-            case 'q':
+            case 'x':
                 exit(0);
             default:
                 int userNum;
                 try {
                     userNum = stoi(userOption);
-                    if(userNum > games.size()){
-                        cout << "Not a valid game";
+                    if(userNum > entrys.size()){
+                        cout << "Not a valid entry";
                     } else {
-                        cout << games.at(userNum - 1).toString() << endl;
+                        cout << entrys.at(userNum - 1).toString() << endl;
                     }
                 } catch (invalid_argument){
                     cout << "Not a valid input " << endl;

347f399ac7bc9950ab33f80f9a17af57d0226b8e
mfwRogue <takahori2@gmail.com>
2023-08-22 22:08:23 -0400

    cleanup

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-        // Value val;
-        // val.SetObject();
-        // val.AddMember("ID", entry.line, doc.GetAllocator());
-        // val.AddMember("name", StringRef(entry.name.c_str()), doc.GetAllocator());
-        // val.AddMember("filepath", StringRef(entry.filepath.c_str()), doc.GetAllocator());
-        // doc.PushBack(val, doc.GetAllocator());
